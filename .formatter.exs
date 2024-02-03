567ff966be201c8b5eaa5894eb0f0f00c712b52b
Calvin Henderson <henderson.calvin.w@gmail.com>
2024-02-03 13:18:29 -0500

    added spark formatter config

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,6 @@
 # Used by "mix format"
 [
   import_deps: [:ash, :ash_postgres],
+  plugins: [Spark.Formatter],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
--- config/config.exs
+++ config/config.exs
@@ -3,6 +3,29 @@ import Config
 config :track_it,
   ecto_repos: [TrackIt.Repo]
 
+# Spark formatter configurition
+
+resource_opts = [
+  type: Ash.Resource,
+  extensions: [],
+  section_order: [
+    :authentication,
+    :token,
+    :resource,
+    :identities,
+    :attributes,
+    :relationships,
+    :calculations,
+    :policies,
+    :postgres
+  ]
+]
+
+config :spark, :formatter,
+  remove_parens?: true,
+  "Ash.Resource": resource_opts,
+  "TrackIt.Resource": resource_opts
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
